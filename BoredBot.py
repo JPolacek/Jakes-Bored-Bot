2a0250d3a717d6503b550bf6057e909be594e490
Jacob Polacek <jacpolacek@gmail.com>
2017-06-16 12:49:40 -0400

    Update BoredBot.py

--- BoredBot.py
+++ BoredBot.py
@@ -6,10 +6,10 @@ import tweepy, time, sys
 argfile = str(sys.argv[1])
 
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = 'hfylmh7EkYBQwuXPvymd3lWBq'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = 'AhAz4KMuyWH58FiNIJ04SMZ0X86Sd5fllreQPvRvrX6tIFTLvt'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = '875559505630810112-Vw7X1FmPIx2l4KNlO6GS6PHbhEj8uRy'#keep the quotes, replace this with your access token
-ACCESS_SECRET = 'rFCF3DUOfCJaHf7n0Yya9ouOFwNMjv8fIT8m9ArQ4NSei'#keep the quotes, replace this with your access token secret
+CONSUMER_KEY = 'C_KEY'#keep the quotes, replace C_KEY with your consumer key
+CONSUMER_SECRET = 'C_SECRET'#keep the quotes, replace C_SECRET with your consumer secret key
+ACCESS_KEY = 'A_KEY'#keep the quotes, replace A_KEY with your access token
+ACCESS_SECRET = 'A_SECRET'#keep the quotes, replace A_SECRET with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
